02181fe3f93d943007b44908eb966a832503d0cb
Ben Richards <blrichards@me.com>
2015-06-18 12:33:46 -0400

    asdf

--- Uploader.rb
+++ Uploader.rb
@@ -18,18 +18,20 @@ class UploadWorker
         fill_in('cred1', :with => 'Benton97')
         click_button 'Login'
         puts "Logged in."
-        Dir.foreach('/root/BNScanner/to_upload') do |logfile|
-            next if logfile == '.' or logfile == '..'
-            path = "/root/BNScanner/to_upload/#{logfile}"
-            sleep(3)
-            puts "Uploading...#{logfile}"
-            click_on 'uploadButton'
-            sleep(2)
-            attach_file('stumblefile', path)
-            sleep(2)
-            find('input[type="submit"]').click
-            sleep(2)
-            visit '/uploads'
+        unless Dir["/foo/bar/*"].empty?
+            Dir.foreach('BNScanner/to_upload') do |logfile|
+                next if logfile == '.' or logfile == '..'
+                path = "BNScanner/to_upload/#{logfile}"
+                sleep(3)
+                puts "Uploading...#{logfile}"
+                click_on 'uploadButton'
+                sleep(2)
+                attach_file('stumblefile', path)
+                sleep(2)
+                find('input[type="submit"]').click
+                sleep(2)
+                visit '/uploads'
+            end
         end
         system('sudo rm /home/pi/BNScanner/to_upload/*')
     end
