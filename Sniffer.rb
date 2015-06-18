b53362b7b62cdf37d9faaae8c9fbafb60d727da8
Ben Richards <blrichards@me.com>
2015-06-18 10:54:40 -0400

    adf

--- Sniffer.rb
+++ Sniffer.rb
@@ -2,18 +2,24 @@ require_relative 'Uploader'
 
 wigle = UploadWorker.new
 
-if `iwconfig`.include? "Nanterre"
-    wigle.upload
-end
+# try to upload if connected to the network
+wigle.upload if `iwconfig`.include? "Nanterre"
 
-if not `iwconfig`.include? "wlan0mon"
+# add a wlan0 subinterface to allow channel hopping
+unless `iwconfig`.include? "wlan0mon"
     system("sudo iw dev wlan0 interface add wlan0mon type monitor")
 end
 
+# cut power to wlan0 (aka the wifi module)
 system "sudo iwconfig wlan0 txpower off"
 sleep 1
+
+# switch wlan0 to monitor mode
 system "sudo iwconfig wlan0 mode monitor"
 sleep 5
+
+# reinitiate power to wlan0
 system "sudo iwconfig wlan0 txpower on"
 
+# trigger kismet_server and place logfiles in to_upload directory
 system "kismet_server -p to_upload"
