355dd4d602eac51cf6eb344b6ad8da44a7ed33bb
Ben Richards <blrichards@me.com>
2015-06-26 14:59:05 -0400

    adf

--- Uploader.rb
+++ Uploader.rb
@@ -12,35 +12,38 @@ class UploadWorker
     include Capybara::DSL
 
     def upload
-        puts "visiting wigle..."
-        visit '/uploads'
-        click_on 'topBarLogin'
-        fill_in('cred0', :with => 'blrichards')
-        fill_in('cred1', :with => 'Benton97')
-        find('.regbutton').click
-	while not page.has_css?('#topBarLogout') do
-	    print "."
-	end
-	# find('.userText')
-        puts "Logged in."
-        unless (Dir.entries('/home/pi/BNScanner/to_upload') - %w{. ..}).empty?
-        # unless (Dir.entries('/Users/benrichards/Projects/BNScanner/to_upload') - %w{. ..}).empty?
-            Dir.foreach('/home/pi/BNScanner/to_upload') do |logfile|
-            # Dir.foreach('/Users/benrichards/Projects/BNScanner/to_upload') do |logfile|
-                next if logfile == '.' or logfile == '..'
-                # path = "/home/pi/BNScanner/to_upload/#{logfile}"
-                path = "/Users/benrichards/Projects/BNScanner/to_upload/#{logfile}"
-                puts "Uploading...#{logfile}"
-                find('#uploadButton').trigger('click')
-                save_and_open_screenshot
-                attach_file('stumblefile', path)
-                # find('stumblefile').attach_file(path)
-                find('input[name="Send"]').click
-                sleep 3 # may need to be increased
-                visit '/uploads'
+        begin
+            print "visiting wigle..."
+            visit '/uploads'
+            click_on 'topBarLogin'
+            fill_in('cred0', :with => 'blrichards')
+            fill_in('cred1', :with => 'Benton97')
+            find('.regbutton').click
+        	while not page.has_css?('#topBarLogout') do
+        	    print "."
+        	end
+            puts "Logged in."
+            unless (Dir.entries('/home/pi/BNScanner/to_upload') - %w{. ..}).empty?
+                Dir.foreach('/home/pi/BNScanner/to_upload') do |logfile|
+                    next if logfile == '.' or logfile == '..'
+                    path = "/home/pi/BNScanner/to_upload/#{logfile}"
+                    puts "Uploading #{logfile}..."
+                    find('#uploadButton').click
+                    find('input[name="stumblefile"]')
+                    attach_file('stumblefile', path)
+                    find('input[name="Send"]').click
+                    while not page.has_css?('#topBarLogout') do
+                	    print "."
+                	end
+                    click_on "Return to your uploads page"
+                    save_and_open_screenshot
+                end
             end
+            system('sudo rm /home/pi/BNScanner/to_upload/Kismet*')
+        # rescue
+            # system('Error occured. Rebooting...')
+            # puts "system will reboot"
         end
-        system('sudo rm /home/pi/BNScanner/to_upload/Kismet*')
     end
 end
 
