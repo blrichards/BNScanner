cfe32c1f7c4df68708516873444c363c986a8a14
Ben Richards <blrichards@me.com>
2015-07-08 20:58:51 +0000

    asdf

--- Sniffer.rb
+++ Sniffer.rb
@@ -2,26 +2,26 @@
 
 require_relative 'Uploader'
 
-puts "Checking for networks"
+puts "Checking for network connection..."
 
 wigle = UploadWorker.new
 wigle.upload("/home/pi/BNScanner/to_upload") if `iwconfig`.include? "Nanterre"
 
 # add a wlan0 subinterface to allow channel hopping
-unless `iwconfig`.include? "wlan0mon"
-    system("sudo iw dev wlan0 interface add wlan0mon type monitor")
-end
+#unless `iwconfig`.include? "wlan0mon"
+#    system("sudo iw dev wlan0 interface add wlan0mon type monitor")
+#end
 
 # cut power to wlan0 (aka the wifi module)
-system "sudo iwconfig wlan0 txpower off"
-sleep 1
+# system "sudo iwconfig wlan0 txpower off"
+# sleep 1
 
 # switch wlan0 to monitor mode
-system "sudo iwconfig wlan0 mode monitor"
-sleep 5
+# system "sudo iwconfig wlan0 mode monitor"
+# sleep 5
 
 # reinitiate power to wlan0
-system "sudo iwconfig wlan0 txpower on"
+# system "sudo iwconfig wlan0 txpower on"
 
 # trigger kismet_server and place logfiles in to_upload directory
 while true
