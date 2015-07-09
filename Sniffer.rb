62665c69a73ab979f874109e1e5edbad9442c138
Ben Richards <blrichards@me.com>
2015-07-09 16:27:29 +0000

    adsf

--- Sniffer.rb
+++ Sniffer.rb
@@ -2,15 +2,15 @@
 require_relative 'Uploader'
 
 if `iwconfig wlan0`.include? "Nanterre"
-    puts "Conventional WiFi dongle detected."
+    puts "\n\n\n\nConventional WiFi dongle detected."
     wigle = UploadWorker.new
     wigle.upload("/home/pi/BNScanner/to_upload")
 elsif `ifconfig eth0`.include? "inet addr:192.168.1"
-    puts "Ethernet connection detected."
+    puts "\n\n\n\nEthernet connection detected."
     wigle = UploadWorker.new
     wigle.upload("/home/pi/BNScanner/to_upload")
 elsif `iwconfig`.include? "wlan8"
-    puts "Wardriving dongle detected.\n\nCommencing data collection..."
+    puts "\n\n\n\nWardriving dongle detected.\nCommencing data collection..."
     system "timeout 120 kismet_server -p /home/pi/BNScanner/to_upload" until false
 else
     puts "No data collection tools detected. Please connect to internet or insert wardriving dongle, then reboot."
